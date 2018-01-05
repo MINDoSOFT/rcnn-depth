709421900b4ca6518cf4a7c64e6953551d8bfd4e
Sergios Stamatis <mindosoft@gmail.com>
2018-01-05 02:47:55 -0500

    Adjust process_frames_parallel.py to not wait at last batch

--- process_frames_parallel.py
+++ process_frames_parallel.py
@@ -42,15 +42,15 @@ specificBatchId = 51;
 for ii in range(0, int(math.ceil(float(cnt)/workers))):
   # Spawn the workers
   for x in range(0, workers):
-    if (x+1+(ii * workers) > cnt - 1):
-      exit()
     batchId = (x+1+(ii * workers))
+    if (batchId > cnt - 1):
+      exit()
     print('BatchId: %d' % (batchId))
     if(not specificWorker or (specificWorker and batchId==specificBatchId)): # adjust if you want only a specific file
-      if (x == (workers-1)):
-        spawn_command = launch_save_frame_batch_wait % (x+1+(ii * workers), operation, matlab_script)
+      if (x == (workers-1) or (batchId == (cnt-1))): # Wait at last worker of current or total batch
+        spawn_command = launch_save_frame_batch_wait % (batchId, operation, matlab_script)
       else:
-        spawn_command = launch_save_frame_batch % (x+1+(ii * workers), operation, matlab_script)
+        spawn_command = launch_save_frame_batch % (batchId, operation, matlab_script)
       if (testing):
         print(spawn_command)
       else:
